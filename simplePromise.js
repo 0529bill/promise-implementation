5f4684a7eb46f9c1160859fd0522ddcdda6cc7e9
bill <0529bilL@gmail.com>
2022-10-11 17:41:08 +0800

    feat: promises

--- simplePromise.js
+++ simplePromise.js
@@ -1,7 +1,17 @@
+//resources:https://github.com/WebDevSimplified/js-promise-library
+
 /**
 new Promise((resolve, reject) => {
 
 }).then((onfullfilled, onrejected) => ... )
+ */
+
+/**
+ 
+then情境1 => 回傳非Promise
+ex, Promise.then(() =>  '11')
+then情境2 => 回傳非Promise
+ex, Promise.then(() =>  Promise('11'))
  */
 
 const STATE = {
@@ -43,17 +53,37 @@ class simplePromise {
   };
 
   #onSuccess(value) {
-    if (this.#state !== STATE.PENDING) return;
-    this.#value = value;
-    this.#state = STATE.FULFILLED;
-    this.#runCallbacks();
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof simplePromise) {
+        value.then(this.#onSuccessBinded, this.#onFailBinded);
+        return;
+      }
+
+      this.#value = value;
+      this.#state = STATE.FULFILLED;
+      this.#runCallbacks();
+    });
   }
 
   #onFail(value) {
-    if (this.#state !== STATE.PENDING) return;
-    this.#value = value;
-    this.#state = STATE.REJECTED;
-    this.#runCallbacks();
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof simplePromise) {
+        value.then(this.#onSuccessBinded, this.#onFailBinded);
+        return;
+      }
+
+      if (this.#catchCallback.length === 0) {
+        throw new UncaughtPromiseError(value);
+      }
+
+      this.#value = value;
+      this.#state = STATE.REJECTED;
+      this.#runCallbacks();
+    });
   }
 
   then(thenCb, catchCb) {
@@ -77,6 +107,18 @@ class simplePromise {
       //     this.#catchCallback.push(catchCallback);
       //   }
 
+      this.#catchCallback.push((result) => {
+        if (!catchCb) {
+          reject(result);
+          return;
+        }
+        try {
+          resolve(catchCb(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
+
       this.#runCallbacks();
     });
 
@@ -84,10 +126,107 @@ class simplePromise {
   }
 
   catch(callback) {
-    this.then(undefined, callback);
+    return this.then(undefined, callback);
   }
 
-  finally(cb) {}
+  finally(callback) {
+    return this.then(
+      (result) => {
+        callback();
+        return result;
+      },
+      (result) => {
+        callback();
+        throw result;
+      }
+    );
+  }
+
+  static resolve(value) {
+    return new Promise((resolve) => {
+      resolve(value);
+    });
+  }
+
+  static reject(value) {
+    return new Promise((resolve, reject) => {
+      reject(value);
+    });
+  }
+
+  static all(promises) {
+    const results = [];
+    const completedPromises = 0;
+    return new simplePromise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+        promise
+          .then((value) => {
+            completedPromises++;
+            result[i] = value;
+            if (completedPromises === promises.length) {
+              resolve(results);
+            }
+          })
+          .catch(reject);
+      }
+    });
+  }
+
+  static allSettled(promises) {
+    const results = [];
+    let completedPromises = 0;
+    return new MyPromise((resolve) => {
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+        promise
+          .then((value) => {
+            results[i] = { status: STATE.FULFILLED, value };
+          })
+          .catch((reason) => {
+            results[i] = { status: STATE.REJECTED, reason };
+          })
+          .finally(() => {
+            completedPromises++;
+            if (completedPromises === promises.length) {
+              resolve(results);
+            }
+          });
+      }
+    });
+  }
+
+  static race(promises) {
+    return new MyPromise((resolve, reject) => {
+      promises.forEach((promise) => {
+        promise.then(resolve).catch(reject);
+      });
+    });
+  }
+
+  static any(promises) {
+    const errors = [];
+    let rejectedPromises = 0;
+    return new MyPromise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+        promise.then(resolve).catch((value) => {
+          rejectedPromises++;
+          errors[i] = value;
+          if (rejectedPromises === promises.length) {
+            reject(new AggregateError(errors, "All promises were rejected"));
+          }
+        });
+      }
+    });
+  }
+}
+
+class UncaughtPromiseError extends Error {
+  constructor(error) {
+    super(error);
+    this.stack = `(in Promise) ${error.stack}`;
+  }
 }
 
 export { simplePromise };
